f3168fcabfba2bd546443527651ac0266c800c9a
william huang <will.h86@gmail.com>
2015-01-06 23:20:24 -0800

    setup gulp npm and initial files

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var watchify = require('watchify');
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
+
+var bundler = watchify(browserify('./src/index.js', watchify.args));
+// add any other browserify options or transforms here
+bundler.transform('brfs');
+
+gulp.task('js', bundle); // so you can run `gulp js` to build the file
+bundler.on('update', bundle); // on any dep update, runs the bundler
+
+function bundle() {
+  return bundler.bundle()
+    // log errors if they happen
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('bundle.js'))
+    // optional, remove if you dont want sourcemaps
+      .pipe(buffer())
+      .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+      .pipe(sourcemaps.write('./')) // writes .map file
+    //
+    .pipe(gulp.dest('./dist'));
+}
